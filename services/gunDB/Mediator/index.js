b5762bed0d6748d36a7d419be6872e860fad8d70
Daniel Lugo <danlugo92@gmail.com>
2019-09-19 11:28:31 -0400

    new gundb service mechanics

--- services/gunDB/Mediator/index.js
+++ services/gunDB/Mediator/index.js
@@ -1,78 +1,16 @@
+const Gun = require("gun");
+// @ts-ignore
+require("gun/sea");
+
 const Action = require("../action-constants.js");
 const API = require("../contact-api/index");
+const Config = require("../config");
 const Event = require("../event-constants");
-const Gun = require("./gun");
+
 /**
  * @typedef {import('../contact-api/SimpleGUN').GUNNode} GUNNode
  * @typedef {import('../contact-api/SimpleGUN').UserGUNNode} UserGUNNode
  */
-
-/**
- * @typedef {object} User
- * @prop {string|null} avatar
- * @prop {string|null} currentHandshakeAddress
- * @prop {string|null} displayName
- * @prop {string} publicKey
- */
-
-/**
- * @type {(() => void)[]}
- */
-const userListeners = [];
-
-/**
- * @type {Record<string, User>}
- */
-const users = {};
-
-/**
- * @param {string} publicKey
- */
-const createUserForListening = publicKey => {
-  if (users[publicKey]) {
-    return;
-  }
-
-  users[publicKey] = {
-    avatar: null,
-    currentHandshakeAddress: null,
-    displayName: null,
-    publicKey
-  };
-
-  const u = Gun.createGun().user(publicKey);
-
-  userListeners.forEach(l => l());
-
-  u.get(API.Key.PROFILE)
-    .get(API.Key.AVATAR)
-    .on(avatar => {
-      // @ts-ignore
-      users[publicKey].avatar = avatar;
-
-      console.log("here1");
-
-      userListeners.forEach(l => l());
-    });
-
-  u.get(API.Key.PROFILE)
-    .get(API.Key.DISPLAY_NAME)
-    .on(displayName => {
-      console.log("here2");
-      // @ts-ignore
-      users[publicKey].displayName = displayName;
-
-      userListeners.forEach(l => l());
-    });
-
-  u.get(API.Key.CURRENT_HANDSHAKE_NODE).on(hn => {
-    console.log("here3");
-    // @ts-ignore
-    users[publicKey].currentHandshakeAddress = hn === null ? null : hn._["#"];
-
-    userListeners.forEach(l => l());
-  });
-};
 
 /**
  * @typedef {object} Emission
@@ -87,6 +25,15 @@ const createUserForListening = publicKey => {
  * @prop {(eventName: string, handler: (data: any) => void) => void} on
  */
 
+/** @type {GUNNode} */
+// @ts-ignore
+const gun = Gun({
+  file: Config.DATA_FILE_NAME,
+  peers: Config.PEERS
+});
+
+const user = gun.user();
+
 module.exports = class Mediator {
   /**
    * @param {Readonly<SimpleSocket>} socket
@@ -99,17 +46,13 @@ module.exports = class Mediator {
     socket.on("disconnect", this.onDisconnect);
 
     socket.on(Action.ACCEPT_REQUEST, this.acceptRequest);
-    socket.on(Action.AUTHENTICATE, this.authenticate);
     socket.on(Action.BLACKLIST, this.blacklist);
     socket.on(Action.GENERATE_NEW_HANDSHAKE_NODE, this.generateHandshakeNode);
-    socket.on(Action.LOGOUT, this.logout);
-    socket.on(Action.REGISTER, this.register);
     socket.on(Action.SEMD_HANDSHAKE_REQUEST, this.sendHandshakeRequest);
     socket.on(Action.SEND_MESSAGE, this.sendMessage);
     socket.on(Action.SET_AVATAR, this.setAvatar);
     socket.on(Action.SET_DISPLAY_NAME, this.setDisplayName);
 
-    socket.on(Event.ON_ALL_USERS, this.onAllUsers);
     socket.on(Event.ON_AVATAR, this.onAvatar);
     socket.on(Event.ON_BLACKLIST, this.onBlacklist);
     socket.on(Event.ON_CHATS, this.onChats);
@@ -120,19 +63,14 @@ module.exports = class Mediator {
   }
 
   /**
-   * @private
-   * @type {(() => void)|null}
-   */
-  userListener = null;
-
-  /**
    * @param {Readonly<{ requestID: string , token: string }>} body
    */
   acceptRequest = body => {
     const { requestID, token } = body;
-    const user = Gun.getUserForToken(token);
 
-    if (user === null) {
+    // TODO: Validate token
+
+    if (!token) {
       this.socket.emit(Action.ACCEPT_REQUEST, {
         ok: false,
         msg: "Token expired.",
@@ -144,8 +82,8 @@ module.exports = class Mediator {
 
     API.Actions.acceptRequest(requestID, user)
       .then(() => {
-        const connectedAndAuthed =
-          this.connected && !!Gun.getUserForToken(token);
+        // TODO: check auth status
+        const connectedAndAuthed = this.connected;
 
         if (connectedAndAuthed) {
           this.socket.emit(Action.ACCEPT_REQUEST, {
@@ -160,7 +98,10 @@ module.exports = class Mediator {
 
           API.Events.onSimplerReceivedRequests(
             receivedRequests => {
-              if (this.connected && !!Gun.getUserForToken(token) && !sent) {
+              // TODO: check auth status
+              const connectedAndAuthed = this.connected;
+
+              if (connectedAndAuthed && !sent) {
                 sent = true;
 
                 this.socket.emit(Event.ON_RECEIVED_REQUESTS, {
@@ -170,7 +111,7 @@ module.exports = class Mediator {
                 });
               }
             },
-            Gun.createGun(),
+            gun,
             user
           );
         }
@@ -187,96 +128,14 @@ module.exports = class Mediator {
   };
 
   /**
-   * @param {Readonly<{ alias: string , pass: string }>} body
-   */
-  authenticate = body => {
-    const { alias, pass } = body;
-
-    const user = Gun.getUserForAlias(alias);
-
-    const emitAuth = () => {
-      try {
-        if (!this.connected) return;
-
-        const token = Gun.getTokenForAlias(alias);
-        const is = user.is;
-
-        if (token === null) {
-          throw new Error("Internal Error: Expected token to not be null.");
-        }
-
-        if (token.length === 0) {
-          throw new Error(
-            "Internal Error: Expected token to be an string of length greater than zero."
-          );
-        }
-
-        if (!is) {
-          throw new Error("Internal Error: user.is undefined");
-        }
-
-        const publicKey = is.pub;
-
-        this.socket.emit(Action.AUTHENTICATE, {
-          ok: true,
-          msg: {
-            publicKey,
-            token
-          },
-          origBody: body
-        });
-      } catch (e) {
-        console.error(`Actions.AUTHENTICATE: ${e.message}`);
-
-        if (!this.connected) return;
-
-        this.socket.emit(Action.AUTHENTICATE, {
-          ok: false,
-          msg: e.message,
-          origBody: body
-        });
-      }
-    };
-
-    if (!!Gun.getTokenForAlias(alias)) {
-      emitAuth();
-      return;
-    }
-
-    API.Actions.authenticate(alias, pass, user)
-      .then(() => {
-        emitAuth();
-
-        if (user.is) {
-          createUserForListening(user.is.pub);
-        } else {
-          console.warn("couldnt get user.is after authenticating");
-        }
-
-        API.Jobs.onAcceptedRequests(API.Events.onSentRequests, user);
-      })
-      .catch(e => {
-        console.error(`Actions.AUTHENTICATE: ${e.message}`);
-
-        if (this.connected) {
-          this.socket.emit(Action.AUTHENTICATE, {
-            ok: false,
-            msg: e.message,
-            origBody: body
-          });
-        }
-      });
-  };
-
-  /**
    * @param {Readonly<{ publicKey: string , token: string }>} body
    */
   blacklist = body => {
     const { publicKey, token } = body;
 
-    const user = Gun.getUserForToken(token);
+    // TODO: Validate token
 
-    if (user === null) {
+    if (!token) {
       this.socket.emit(Action.BLACKLIST, {
         ok: false,
         msg: "Token expired.",
@@ -316,9 +175,9 @@ module.exports = class Mediator {
    */
   generateHandshakeNode = body => {
     const { token } = body;
-    const user = Gun.getUserForToken(token);
+    // TODO: Validate token
 
-    if (user === null) {
+    if (!token) {
       this.socket.emit(Action.GENERATE_NEW_HANDSHAKE_NODE, {
         ok: false,
         msg: "Token expired.",
@@ -328,7 +187,7 @@ module.exports = class Mediator {
       return;
     }
 
-    API.Actions.generateNewHandshakeNode(Gun.createGun(), user)
+    API.Actions.generateNewHandshakeNode(gun, user)
       .then(() => {
         if (this.connected) {
           this.socket.emit(Action.GENERATE_NEW_HANDSHAKE_NODE, {
@@ -342,73 +201,6 @@ module.exports = class Mediator {
         if (this.connected) {
           this.socket.emit(Action.GENERATE_NEW_HANDSHAKE_NODE, {
             ok: true,
-            msg: e.message,
-            origBody: body
-          });
-        }
-      });
-  };
-
-  /**
-   * @param {Readonly<{ token: string }>} body
-   */
-  logout = body => {
-    const { token } = body;
-    const user = Gun.getUserForToken(token);
-
-    if (user === null) {
-      this.socket.emit(Action.LOGOUT, {
-        ok: false,
-        msg: "Token expired.",
-        origBody: body
-      });
-
-      return;
-    }
-
-    API.Actions.logout(user)
-      .then(() => {
-        if (this.connected) {
-          this.socket.emit(Action.LOGOUT, {
-            ok: true,
-            msg: null,
-            origBody: body
-          });
-        }
-      })
-      .catch(e => {
-        if (this.connected) {
-          this.socket.emit(Action.LOGOUT, {
-            ok: false,
-            msg: e.message,
-            origBody: body
-          });
-        }
-      });
-  };
-
-  /**
-   * @param {Readonly<{ alias: string , pass: string }>} body
-   */
-  register = body => {
-    const { alias, pass } = body;
-
-    const user = Gun.createGun().user();
-
-    API.Actions.register(alias, pass, user)
-      .then(() => {
-        if (this.connected) {
-          this.socket.emit(Action.REGISTER, {
-            ok: true,
-            msg: null,
-            origBody: body
-          });
-        }
-      })
-      .catch(e => {
-        if (this.connected) {
-          this.socket.emit(Action.REGISTER, {
-            ok: false,
             msg: e.message,
             origBody: body
           });
@@ -421,9 +213,9 @@ module.exports = class Mediator {
    */
   sendHandshakeRequest = body => {
     const { handshakeAddress, recipientPublicKey, token } = body;
-    const user = Gun.getUserForToken(token);
+    // TODO: Validate token
 
-    if (user === null) {
+    if (!token) {
       this.socket.emit(Action.SEMD_HANDSHAKE_REQUEST, {
         ok: false,
         msg: "Token expired.",
@@ -438,7 +230,7 @@ module.exports = class Mediator {
     API.Actions.sendHandshakeRequest(
       handshakeAddress,
       recipientPublicKey,
-      Gun.createGun(),
+      gun,
       user
     )
       .then(() => {
@@ -466,9 +258,9 @@ module.exports = class Mediator {
    */
   sendMessage = reqBody => {
     const { body, recipientPublicKey, token } = reqBody;
-    const user = Gun.getUserForToken(token);
+    // TODO: Validate token
 
-    if (user === null) {
+    if (!token) {
       this.socket.emit(Action.SEND_MESSAGE, {
         ok: false,
         msg: "Token expired.",
@@ -507,9 +299,9 @@ module.exports = class Mediator {
    */
   setAvatar = body => {
     const { avatar, token } = body;
-    const user = Gun.getUserForToken(token);
+    // TODO: Validate token
 
-    if (user === null) {
+    if (!token) {
       this.socket.emit(Action.SET_AVATAR, {
         ok: false,
         msg: "Token expired.",
@@ -545,9 +337,9 @@ module.exports = class Mediator {
    */
   setDisplayName = body => {
     const { displayName, token } = body;
-    const user = Gun.getUserForToken(token);
+    // TODO: Validate token
 
-    if (user === null) {
+    if (!token) {
       this.socket.emit(Action.SET_DISPLAY_NAME, {
         ok: false,
         msg: "Token expired.",
@@ -583,52 +375,12 @@ module.exports = class Mediator {
   /**
    * @param {Readonly<{ token: string }>} body
    */
-  onAllUsers = body => {
-    const { token } = body;
-
-    const user = Gun.getUserForToken(token);
-
-    if (user === null) {
-      this.socket.emit(Event.ON_ALL_USERS, {
-        ok: false,
-        msg: "Token expired.",
-        origBody: body
-      });
-
-      return;
-    }
-
-    this.userListener = () => {
-      if (this.connected) {
-        this.socket.emit(Event.ON_ALL_USERS, {
-          ok: true,
-          msg: Object.values(users).filter(u => {
-            if (user.is) {
-              return u.publicKey !== user.is.pub;
-            } else {
-              console.error("u.is undefined");
-              return true;
-            }
-          }),
-          origBody: body
-        });
-      }
-    };
-
-    this.userListener();
-
-    userListeners.push(this.userListener);
-  };
-
-  /**
-   * @param {Readonly<{ token: string }>} body
-   */
   onAvatar = body => {
     const { token } = body;
 
-    const user = Gun.getUserForToken(token);
+    // TODO: Validate token
 
-    if (user === null) {
+    if (!token) {
       this.socket.emit(Event.ON_AVATAR, {
         ok: false,
         msg: "Token expired.",
@@ -639,7 +391,7 @@ module.exports = class Mediator {
     }
 
     API.Events.onAvatar(avatar => {
-      if (this.connected && !!Gun.getUserForToken(token)) {
+      if (this.connected && !!token) {
         this.socket.emit(Event.ON_AVATAR, {
           msg: avatar,
           ok: true,
@@ -655,9 +407,9 @@ module.exports = class Mediator {
   onBlacklist = body => {
     const { token } = body;
 
-    const user = Gun.getUserForToken(token);
+    // TODO: Validate token
 
-    if (user === null) {
+    if (!token) {
       this.socket.emit(Event.ON_BLACKLIST, {
         ok: false,
         msg: "Token expired.",
@@ -668,7 +420,8 @@ module.exports = class Mediator {
     }
 
     API.Events.onBlacklist(blacklist => {
-      if (this.connected && !!Gun.getUserForToken(token)) {
+      // TODO: Validate token
+      if (this.connected && !!token) {
         this.socket.emit(Event.ON_BLACKLIST, {
           msg: blacklist,
           ok: true,
@@ -684,9 +437,9 @@ module.exports = class Mediator {
   onChats = body => {
     const { token } = body;
 
-    const user = Gun.getUserForToken(token);
+    // TODO: Validate token
 
-    if (user === null) {
+    if (!token) {
       this.socket.emit(Event.ON_CHATS, {
         ok: false,
         msg: "Token expired.",
@@ -698,7 +451,8 @@ module.exports = class Mediator {
 
     API.Events.onChats(
       chats => {
-        if (this.connected && !!Gun.getUserForToken(token)) {
+        // TODO: Validate token
+        if (this.connected && !!token) {
           this.socket.emit(Event.ON_CHATS, {
             msg: chats,
             ok: true,
@@ -706,7 +460,7 @@ module.exports = class Mediator {
           });
         }
       },
-      Gun.createGun(),
+      gun,
       user
     );
   };
@@ -717,9 +471,9 @@ module.exports = class Mediator {
   onDisplayName = body => {
     const { token } = body;
 
-    const user = Gun.getUserForToken(token);
+    // TODO: Validate token
 
-    if (user === null) {
+    if (!token) {
       this.socket.emit(Event.ON_DISPLAY_NAME, {
         ok: false,
         msg: "Token expired.",
@@ -730,7 +484,7 @@ module.exports = class Mediator {
     }
 
     API.Events.onDisplayName(displayName => {
-      if (this.connected && !!Gun.getUserForToken(token)) {
+      if (this.connected && !!token) {
         this.socket.emit(Event.ON_DISPLAY_NAME, {
           msg: displayName,
           ok: true,
@@ -746,9 +500,9 @@ module.exports = class Mediator {
   onHandshakeAddress = body => {
     const { token } = body;
 
-    const user = Gun.getUserForToken(token);
+    // TODO: Validate token
 
-    if (user === null) {
+    if (!token) {
       this.socket.emit(Event.ON_HANDSHAKE_ADDRESS, {
         ok: false,
         msg: "Token expired.",
@@ -759,7 +513,8 @@ module.exports = class Mediator {
     }
 
     API.Events.onCurrentHandshakeAddress(addr => {
-      if (this.connected && !!Gun.getUserForToken(token)) {
+      // TODO: Validate token
+      if (this.connected && !!token) {
         this.socket.emit(Event.ON_HANDSHAKE_ADDRESS, {
           ok: true,
           msg: addr,
@@ -775,9 +530,9 @@ module.exports = class Mediator {
   onReceivedRequests = body => {
     const { token } = body;
 
-    const user = Gun.getUserForToken(token);
+    // TODO: Validate token
 
-    if (user === null) {
+    if (!token) {
       this.socket.emit(Event.ON_RECEIVED_REQUESTS, {
         ok: false,
         msg: "Token expired.",
@@ -789,7 +544,8 @@ module.exports = class Mediator {
 
     API.Events.onSimplerReceivedRequests(
       receivedRequests => {
-        if (this.connected && !!Gun.getUserForToken(token)) {
+        // TODO: Validate token
+        if (this.connected && !!token) {
           this.socket.emit(Event.ON_RECEIVED_REQUESTS, {
             msg: receivedRequests,
             ok: true,
@@ -797,7 +553,7 @@ module.exports = class Mediator {
           });
         }
       },
-      Gun.createGun(),
+      gun,
       user
     );
   };
@@ -808,9 +564,9 @@ module.exports = class Mediator {
   onSentRequests = body => {
     const { token } = body;
 
-    const user = Gun.getUserForToken(token);
+    // TODO: Validate token
 
-    if (user === null) {
+    if (!token) {
       this.socket.emit(Event.ON_SENT_REQUESTS, {
         ok: false,
         msg: "Token expired.",
@@ -822,7 +578,8 @@ module.exports = class Mediator {
 
     API.Events.onSimplerSentRequests(
       sentRequests => {
-        if (this.connected && !!Gun.getUserForToken(token)) {
+        // TODO: Validate token
+        if (this.connected && !!token) {
           this.socket.emit(Event.ON_SENT_REQUESTS, {
             msg: sentRequests,
             ok: true,
@@ -830,7 +587,7 @@ module.exports = class Mediator {
           });
         }
       },
-      Gun.createGun(),
+      gun,
       user
     );
   };
